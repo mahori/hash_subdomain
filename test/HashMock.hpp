94a9409b9846f08b03078a6a46b03b65aea14f03
mahori <mahori@users.noreply.github.com>
2018-10-15 18:48:17 +0900

    Hash計算周りをリファクタリング
    変数名・関数名をできるだけわかりやすくした

--- test/HashMock.hpp
+++ test/HashMock.hpp
@@ -8,10 +8,18 @@
 class HashMock
 {
 public:
-  HashMock(void) =default;
+  HashMock(std::size_t size = std::numeric_limits<std::size_t>::max())
+    : size_(size)
+  {
+  }
+
   ~HashMock(void) =default;
 
-  MOCK_CONST_METHOD2(hash, std::size_t(const std::string&, std::size_t));
+  MOCK_CONST_METHOD1(hash, std::size_t(const std::string&));
+  MOCK_CONST_METHOD0(size, std::size_t(void));
+
+private:
+  std::size_t size_;
 };
 
 #endif  // HASH_MOCK_H
