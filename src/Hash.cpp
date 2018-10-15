2d70f55fcaba67ba4f1da1fe05f818d03c7317f6
mahori <mahori@users.noreply.github.com>
2018-10-15 13:25:02 +0900

    Hashクラスの仕様を変更
    実際にhashを求めるときに最大値を指定するようにした

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -1,26 +1,17 @@
 #include "Hash.hpp"
 #include <cstddef>
 #include <functional>
+#include <stdexcept>
+#include <string>
 
-Hash::Hash(std::size_t max)
-  : max_(max)
+std::size_t Hash::hash(const std::string& text, std::size_t max) const
 {
-}
+  if (max == 0) {
+    throw std::invalid_argument("Variable max is 0");
+  }
 
-std::size_t Hash::hash(const std::string& text) const
-{
   std::size_t result = std::hash<std::string>()(text);
-  result %= max_;
+  result %= max;
 
   return result;
 };
-
-std::size_t Hash::max(void) const
-{
-  return max_;
-}
-
-void Hash::setMax(size_t value)
-{
-  max_ = value;
-}
--- src/Hash.hpp
+++ src/Hash.hpp
@@ -8,16 +8,10 @@
 class Hash
 {
 public:
-  Hash(std::size_t max = std::numeric_limits<std::size_t>::max());
+  Hash(void) =default;
   virtual ~Hash(void) =default;
 
-  virtual std::size_t hash(const std::string& text) const;
-
-  virtual std::size_t max(void) const;
-  virtual void setMax(size_t value);
-
-private:
-  std::size_t max_;
+  virtual std::size_t hash(const std::string& text, std::size_t max = std::numeric_limits<std::size_t>::max()) const;
 };
 
 #endif  // HASH_H
