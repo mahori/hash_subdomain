b882214b054e0a075f1fcb67c253ac0848383ec1
mahori <mahori@users.noreply.github.com>
2018-10-16 09:00:55 +0900

    Hash.cppを調整

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -4,23 +4,28 @@
 #include <stdexcept>
 #include <string>
 
-Hash::Hash(std::size_t size)
+using invalid_argument = std::invalid_argument;
+using size_t           = std::size_t;
+using string           = std::string;
+using string_hash      = std::hash<std::string>;
+
+Hash::Hash(size_t size)
   : size_(size)
 {
   if (size_ == 0) {
-    throw std::invalid_argument("hash size is 0");
+    throw invalid_argument("hash size is 0");
   }
 }
 
-std::size_t Hash::hash(const std::string& text) const
+size_t Hash::hash(const string& text) const
 {
-  std::size_t result = std::hash<std::string>()(text);
-  result %= size_;
+  size_t hash = string_hash()(text);
+  hash %= size_;
 
-  return result;
+  return hash;
 }
 
-std::size_t Hash::size(void) const
+size_t Hash::size(void) const
 {
   return size_;
 }
