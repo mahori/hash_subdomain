bafe28cc3659022838ff8f7b8f926b5deb4c4085
mahori <mahori@users.noreply.github.com>
2018-10-16 10:26:09 +0900

    Utils関数群を調整

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,8 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <cstddef>
+#include <string>
+
 extern std::string get_alphabets(std::size_t hash_size, std::size_t hash_value);
-extern std::size_t number_of_alphabets(void);
 extern std::size_t hash_size(std::size_t length);
 
 #endif  // UTILS_H 
